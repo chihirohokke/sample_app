ab97f1c3beaa75a62508257bfdfd6d68831b5d9a
Ubuntu <ubuntu@ip-172-31-82-47.ec2.internal>
2020-05-15 08:32:58 +0000

    fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://hokke-s7.herokuapp.com'
+  host = 'https://sample-app-hokke.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
